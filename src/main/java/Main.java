f2a8e00f31f843c59c75ad32ed1e7fb2eb4f5f16
RonaldWigginton <ronniewigginton@gmail.com>
2020-03-14 13:00:28 -0500

    Parser Assignment

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+    Author: Ronnie Wigginton
+    Project:
+
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String equation1 = "A@(2*(A-C*D))+(9*B/(2*C+1)-B*3)+E%(F-A)";
+        String equation2 = "B*(3@(A-D)%(B-C@D))+4@D*2";
+        Parser parser = new Parser(equation1);
+    }
+}
